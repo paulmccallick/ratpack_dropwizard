e8785bff53a0f201041326b357dc490e01a7aaa9
Paul McCallick <paul@mccallick.com>
2016-05-21 19:27:53 -0700

    now works with my dockerized graphite instance

--- src/main/java/dropwizardtest/app/Main.java
+++ src/main/java/dropwizardtest/app/Main.java
@@ -1,6 +1,10 @@
 package dropwizardtest.app;
 
 
+import java.net.InetSocketAddress;
+
+import com.codahale.metrics.graphite.Graphite;
+
 import ratpack.dropwizard.metrics.DropwizardMetricsModule;
 import ratpack.guice.Guice;
 import ratpack.server.RatpackServer;
@@ -16,8 +20,15 @@ public class Main {
     .start(server -> server
         .registry(
             Guice.registry(r -> r
-                .module(DropwizardMetricsModule.class,
-                    c -> c.console(con -> con.enable(true)
+                .module(DropwizardMetricsModule.class, metrics -> metrics
+                    .jvmMetrics(true)
+                    .graphite(g -> g
+                        .enable()
+                        .reporterInterval(java.time.Duration.ofSeconds(1))
+                        .sender(new Graphite(new InetSocketAddress("192.168.99.100", 2003)))
+                        .prefix("dropwizardtest."))
+                    .console(con -> con
+                        .enable(true)
                         .reporterInterval(java.time.Duration.ofSeconds(3))))))
         .handlers(chain -> chain
             .get(ctx -> ctx.render("Hello World!"))));
